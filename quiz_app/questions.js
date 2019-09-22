21ab54a2f3dcfb3fbf4088c0e380ddf1ab040d1e
Abel Martinez <abel.martinez9410@gmail.com>
2019-09-21 19:34:52 -0500

    Added initial files.

---
+++ quiz_app/questions.js
@@ -0,0 +1,67 @@
+let questions = [
+    {question: "During the initial Licensing Phase, a teen driver is only allowed two passengers under age 20 in the vehicle during the first 12 months of licensing",
+     answers: ['true', 'false'],
+     correctAns: 'false'   
+    },
+
+    {question: "How should a driver proceed within an intersection waiting to make a left turn and the traffic signal light turns red?",
+    answers: ['a.Wait at the intersection until the light turns green.', 'b. Yield to oncoming traffic and complete the turn', 'c. Make sure it is clear, then back up from the intersection'],
+    correctAns: 'b. Yield to oncoming traffic and complete the turn'       
+    },
+
+    {question: "In Order to Obtain court supervision for a traffic violation, a minor must appear in court with a parent/legal guardian and attend traffic safety school.",
+    answers: ['true', 'false'],
+    correctAns: 'True'    
+    },  
+
+    {question: "A parent or legal guardian may request that the driver's license of a minor be canceled at any time prior to age 18.",
+    answers: ['true', 'false'],
+    correctAns: 'True'    
+    },
+
+    {question: "When passing another vehicle, a driver should wait until the entire car the driver just passed is visible in the rearview mirror before turning back into the right-hand lane.",
+    answers: ['true', 'false'],
+    correctAns: 'True'    
+    },
+
+    {question: "After making the proper stop, yielding to traffic or pedestrians within the intersection, it is permissible for drivers on a one-way street to turn left at a red light onto another one-way street that moves traffic to left.",
+    answers: ['true', 'false'],
+    correctAns: 'True'    
+    },
+    
+    {question: "When on a two-lane roadway, drivers must stop their vehicles when approaching a stopped school bus with its warning lights flashing and its stop signal arm extended.",
+    answers: ['true', 'false'],
+    correctAns: 'True'    
+    },
+
+    {question: "When an authorized vehicle using its sirens and flashing lights approaches a vehicle, the driver should pull to the right-hand edge of the roadway and wait for the emergency vehicle to pass.",
+     answers: ['true', 'false'],
+    correctAns: 'True'    
+    },
+   
+    {question: "What is the penalty for being convicted of illegally passing a stopped school bus?",
+    answers: ['a. A suspension of driving privileges.', 'b. A suspension of the vehicle registration.','c. A $30 fine.'],
+    correctAns: 'a. A suspension of driving privileges.'    
+    }, 
+
+    {question: "A driver may pass another vehicle by driving on the shoulder of the road.",
+    answers: ['true', 'false'],
+    correctAns: 'false'   
+   },  
+
+   {question: "It is permissible to make a right turn against a red-signal light after stopping and yielding to other vehicles and pedestrians.",
+   answers: ['true', 'false'],
+  correctAns: 'True'    
+  },
+
+  {question: "A driver must give the right- or left-turn signal when changing lanes.",
+  answers: ['true', 'false'],
+ correctAns: 'True'    
+ },
+
+ {question: "In urban areas, drivers moving out of an alley, building, private road, or driveway need not come to a complete stop before entering the roadway if the roadway is clear of traffic.",
+ answers: ['true', 'false'],
+ correctAns: 'false'   
+},  
+
+];
