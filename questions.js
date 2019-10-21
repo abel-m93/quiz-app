7d9bfd29f5674d58b1576cf3397bf4296a10b68f
Abel <abel.martinez9410@gmail.com>
2019-10-20 20:59:51 -0500

    Added decision structures

---
+++ questions.js
@@ -0,0 +1,81 @@
+let questions = [
+    {question: "During the initial Licensing Phase, a teen driver is only allowed two passengers under age 20 in the vehicle during the first 12 months of licensing",
+     answers: ['true', 'false'],
+     correctAns: 'false'   
+    },
+
+    {question: "How should a driver proceed within an intersection waiting to make a left turn and the traffic signal light turns red?",
+    answers: ['a.  Wait at the intersection until the light turns green.', 'b.  Yield to oncoming traffic and complete the turn', 'c.  Make sure it is clear, then back up from the intersection'],
+    correctAns: 'b.  Yield to oncoming traffic and complete the turn'       
+    },
+
+    {question: "In Order to Obtain court supervision for a traffic violation, a minor must appear in court with a parent/legal guardian and attend traffic safety school.",
+    answers: ['true', 'false'],
+    correctAns: 'true'    
+    },  
+
+    {question: "A parent or legal guardian may request that the driver's license of a minor be canceled at any time prior to age 18.",
+    answers: ['true', 'false'],
+    correctAns: 'true'    
+    },
+
+    {question: "When passing another vehicle, a driver should wait until the entire car the driver just passed is visible in the rearview mirror before turning back into the right-hand lane.",
+    answers: ['true', 'false'],
+    correctAns: 'true'    
+    },
+
+    {question: "After making the proper stop, yielding to traffic or pedestrians within the intersection, it is permissible for drivers on a one-way street to turn left at a red light onto another one-way street that moves traffic to left.",
+    answers: ['true', 'false'],
+    correctAns: 'true'    
+    },
+    
+    {question: "When on a two-lane roadway, drivers must stop their vehicles when approaching a stopped school bus with its warning lights flashing and its stop signal arm extended.",
+    answers: ['true', 'false'],
+    correctAns: 'true'    
+    },
+
+    {question: "When an authorized vehicle using its sirens and flashing lights approaches a vehicle, the driver should pull to the right-hand edge of the roadway and wait for the emergency vehicle to pass.",
+     answers: ['true', 'false'],
+    correctAns: 'true'    
+    },
+   
+    {question: "What is the penalty for being convicted of illegally passing a stopped school bus?",
+    answers: ['a.  A suspension of driving privileges.', 'b.  A suspension of the vehicle registration.','c. A $30 fine.'],
+    correctAns: 'a.  A suspension of driving privileges.'    
+    }, 
+
+    {question: "A driver may pass another vehicle by driving on the shoulder of the road.",
+    answers: ['true', 'false'],
+    correctAns: 'false'   
+    },  
+
+   {question: "It is permissible to make a right turn against a red-signal light after stopping and yielding to other vehicles and pedestrians.",
+   answers: ['true', 'false'],
+   correctAns: 'true'    
+   },
+
+  {question: "A driver must give the right- or left-turn signal when changing lanes.",
+  answers: ['true', 'false'],
+  correctAns: 'true'    
+  },
+
+  {question: "In urban areas, drivers moving out of an alley, building, private road, or driveway need not come to a complete stop before entering the roadway if the roadway is clear of traffic.",
+  answers: ['true', 'false'],
+  correctAns: 'false'   
+  },
+
+  {question: "Illinois drivers under age 16 are required to have at least 50 hours of behind-the-wheel supervised driving before obtaining their driver’s license.",
+  answers: ['true', 'false'],
+  correctAns: 'true'   
+  },
+
+  {question: "Drivers under age 20 are prohibited from using their cellphone while driving except in the case of an emergency",
+  answers: ['true', 'false'],
+  correctAns: 'false'   
+  },
+
+  {question: "Drivers under age 18 must hold their instruction permit for nine consecutive months prior to obtaining their driver’s license.",
+  answers: ['true', 'false'],
+  correctAns: 'true'   
+  },
+];
